ed24d4dfd3075f755a150eb9c206466a0a1f3421
Matko <ljepovicmatko@gmail.com>
2025-02-28 09:53:19 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Matko\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Matko\\apps\\vscPrvi\\prvi" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Matko\\Flutter"
+  "PROJECT_DIR=C:\\Matko\\apps\\vscPrvi\\prvi"
+  "FLUTTER_ROOT=C:\\Matko\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Matko\\apps\\vscPrvi\\prvi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Matko\\apps\\vscPrvi\\prvi"
+  "FLUTTER_TARGET=C:\\Matko\\apps\\vscPrvi\\prvi\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Matko\\apps\\vscPrvi\\prvi\\.dart_tool\\package_config.json"
+)
